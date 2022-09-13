3371335a1a05a5624195b051d603fba666e5fb4c
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-12 23:23:07 -0300

    Add files via upload

---
+++ desigualdade_economica_genero.R
@@ -0,0 +1,18 @@
+
+# Desigualdade econômica entre gêneros -----------------------------------------------------------------------------------------------------
+# Auotria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 12/09/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/economic-inequality-by-gender ---------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Nós apresentamos aqui dados sobre desigualdade econômica entre homens e
+### mulheres. Nós também discutimos como essas desigualdades tem mudado ao
+### longo do tempo.
+
+### Embora as desigualdades de gêneros permaneçam comuns e grandes, hoje
+### são menores do que era algumas décadas atrás.
+
+
+
+
